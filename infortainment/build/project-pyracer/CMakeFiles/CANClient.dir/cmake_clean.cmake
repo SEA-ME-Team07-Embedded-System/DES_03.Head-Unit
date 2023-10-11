c8653ce4c5b35750606644d31c41a28a5e5e871a
Junho Kim <73748884+junho2000@users.noreply.github.com>
2023-10-11 18:45:29 +0200

    change qt app compile with cmake
    hu, dashboard, someip service client simultaneously build with cmake

---
+++ infortainment/build/project-pyracer/CMakeFiles/CANClient.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CANClient"
+  "CANClient.pdb"
+  "CMakeFiles/CANClient.dir/src-gen/v1/commonapi/CANSomeIPDeployment.cpp.o"
+  "CMakeFiles/CANClient.dir/src-gen/v1/commonapi/CANSomeIPProxy.cpp.o"
+  "CMakeFiles/CANClient.dir/src/CANClient.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CANClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
